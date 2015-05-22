cc09c7b81e91e6365fe6732d96637b683f8895e5
Juan José Salvador <juanjosalvador@openmailbox.org>
2015-05-22 10:20:53 +0200

    Update now-playing.sh

--- now-playing.sh
+++ now-playing.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-play=`mocp -i | grep SongTitle | cut -c 12-` 
+# moc-twitter-sharing script, by Juanjo Salvador Piedra (aka SrFreak)
+# for MOC Player community. Written in Shell.
+#
+# Website: http://juanjosalvador.github.io/
+# Repository and how-to: https://github.com/JuanjoSalvador/moc-twitter-sharing
+
+playing=`mocp -i | grep SongTitle | cut -c 12-` 
 artist=`mocp -i | grep Artist | cut -c 9-`
 
-t update "#NowPlaying $artist - $play"
+t update "#NowPlaying $artist - $playing"
